e74f5605aaf61932ac5a36516722c26e23b26047
Nick Tsai <nicktsai1026@gmail.com>
2017-06-20 18:09:13 +0800

    change portfolio website

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-#Portfolio Site
-
-This is the first website I have ever done. This site consists of two parts: 
-  - an introduction of myself (background, hobbies, skills)
-  - a photo album to illustrate the exciting experience I had in Australia during Working Holiday
-
-#Responsiveness
-
-To acheive the responsiveness across different resolution (from mobile to desktop), I use the Bootstrap framework. 
-
-#Photo Album with paginations
-
-I use the jQuery to control the album and switch the photos. I use the buttom to click function removeClass and addClass.
-
+#Portfolio
--- script.js
+++ script.js
@@ -1,36 +1,27 @@
 $( document ).ready(function() {
+	$( ".navbar-toggle .collapsed" ).click(function() {
+		$(".collapse .navbar-collapse").slideDown("slow");
+	});
+  $( ".navbar-toggle .collapsed" ).click(function() {
+		$(".collapse .navbar-collapse").slideUp("slow");
+	});
+  $( ".page-scroll" ).click(function() {
+		$(".navbar-collapse").slideUp("slow");
+	});
+	
+	$( "#button1" ).click(function() {
+		$(".plTwo,.prTwo,.plThree,.prThree").addClass("hide");
+	    $(".plOne,.prOne").removeClass("hide");
+	});
 	$( "#button2" ).click(function() {
-		$(".pageOne,.pageThree,.pageFour,.pageFive,.pageSix,.pageEight,.pageSeven").addClass("hide");
-	    $(".pageTwo").removeClass("hide");
+		$(".plOne,.prOne,.plThree,.prThree").addClass("hide");
+	    $(".plTwo,.prTwo").removeClass("hide");
 	});
 	$( "#button3" ).click(function() {
-		$(".pageOne,.pageTwo,.pageFour,.pageFive,.pageSix,.pageEight,.pageSeven").addClass("hide");
-	    $(".pageThree").removeClass("hide");
+		$(".plOne,.prOne,.plTwo,.prTwo").addClass("hide");
+	    $(".plThree,.prThree").removeClass("hide");
 	});
-	$( "#button1" ).click(function() {
-		$(".pageThree,.pageTwo,.pageFour,.pageFive,.pageSix,.pageSeven,.pageEight").addClass("hide");
-	    $(".pageOne").removeClass("hide");
-	});
-	$( "#button4" ).click(function() {
-		$(".pageThree,.pageTwo,.pageOne,.pageFive,.pageSix,.pageSeven,.pageEight").addClass("hide");
-	    $(".pageFour").removeClass("hide");
-	});
-	$( "#button5" ).click(function() {
-		$(".pageThree,.pageTwo,.pageOne,.pageFour,.pageSix,.pageSeven,.pageEight").addClass("hide");
-	    $(".pageFive").removeClass("hide");
-	});
-	$( "#button6" ).click(function() {
-		$(".pageThree,.pageTwo,.pageOne,.pageFour,.pageFive,.pageSeven,.pageEight").addClass("hide");
-	    $(".pageSix").removeClass("hide");
-	});
-	$( "#button7" ).click(function() {
-		$(".pageThree,.pageTwo,.pageOne,.pageFour,.pageFive,.pageSix,.pageEight").addClass("hide");
-	    $(".pageSeven").removeClass("hide");
-	});
-	$( "#button8" ).click(function() {
-		$(".pageThree,.pageTwo,.pageOne,.pageFour,.pageFive,.pageSix,.pageSeven").addClass("hide");
-	    $(".pageEight").removeClass("hide");
-	});
+
 
     console.log( "ready!" );
 });
